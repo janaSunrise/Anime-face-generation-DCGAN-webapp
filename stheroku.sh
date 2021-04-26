5e8948d65ab39a6c4b4021ab90d5298adbd1d225
janaSunrise <warriordefenderz@gmail.com>
2021-04-26 21:22:03 +0530

    Change theming.

--- stheroku.sh
+++ stheroku.sh
@@ -4,4 +4,9 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+primaryColor=\"#0cf1ed\"\n\
+backgroundColor=\"#23272a\"\n\
+secondaryBackgroundColor=\"#2c2f33\"\n\
+textColor=\"#fafafa\"\n\
 " > ~/.streamlit/config.toml
