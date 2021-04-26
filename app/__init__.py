43a48436614c3c894487fdc86fcce7ef5299d231
janaSunrise <warriordefenderz@gmail.com>
2021-04-26 18:02:42 +0530

    Added the network, and constants.

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+LATENT_SIZE, BATCH_SIZE = 128, 128
+STATS = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+SAVE_DIR = "generated"
---
+++ app/model.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+
+from . import LATENT_SIZE
+
+discriminator = nn.Sequential(
+    nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(512),
+    nn.LeakyReLU(0.2, inplace=True),
+
+    nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+
+    nn.Flatten(),
+    nn.Sigmoid()
+)
+
+generator = nn.Sequential(
+    nn.ConvTranspose2d(LATENT_SIZE, 512, kernel_size=4, stride=1, padding=0, bias=False),
+    nn.BatchNorm2d(512),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(256),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(128),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.BatchNorm2d(64),
+    nn.ReLU(True),
+
+    nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+    nn.Tanh()
+)
